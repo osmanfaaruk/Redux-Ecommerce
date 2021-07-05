de50f9200977985d07d753772c648a200ed57e2d
Md. Osman Faruk <programmming.code@gmail.com>
2021-07-05 19:46:33 +0600

    working

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Navbar.css';
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="container">
+                <div className="nav__container">
+                    <div className="nav__left">
+                        <img src='/images/logo.webp' alt="logo" />
+                    </div>
+                    <div className="nav__right">
+
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Navbar;
