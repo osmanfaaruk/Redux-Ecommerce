1a1d168a6f07f197335a873752485924a100bbcd
Md. Osman Faruk <programmming.code@gmail.com>
2021-07-05 21:30:12 +0600

    working

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import {
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home';
 import OrderedItem from './components/OrderedItem/OrderedItem';
+import CardDetails from './components/CardDetails/CardDetails';
 function App() {
   return (
     <Router>
       <Navbar/>
       <Route path="/" exact component={Home} />
-      <Router path="/orderedItem" component={OrderedItem}/>
+      <Router path="/orderedItem" exact component={OrderedItem}/>
+      <Router path="/productDetails/:id" exact component={CardDetails}/>
     </Router>
   );
 }
--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import './ProductCard.css';
 const ProductCard = () => {
     const {products} = useSelector((state)=> state.ProductReducer)
@@ -10,7 +11,9 @@ const ProductCard = () => {
                 <div className="col-3" key={product.id}> 
                     <div className="product">
                         <div className="product__img">
-                            <img src={`/images/${product.image}`} alt="image name" />
+                           <Link to={`/productDetails/${product.id}`}>
+                           <img src={`/images/${product.image}`} alt="image name" />
+                           </Link>
                         </div>
                         <div className="product__name">
                             {product.name}
@@ -18,12 +21,13 @@ const ProductCard = () => {
                         <div className="row">
                             <div className="col-6">
                                 <div className="product__price">
-                                    {product.price}
+                                    ${product.price}
+                                    <span className="discount">{product.discount}%</span>
                                 </div>
                             </div>
                             <div className="col-6">
                                 <div className="product__discount__price">
-                                    {product.discountPrice}
+                                    ${product.discountPrice}
                                 </div>
                             </div>
                         </div>
